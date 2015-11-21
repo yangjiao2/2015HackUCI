2c210704108b668939d9a3e4e3ba40d6a2dab3ba
JP <parkjpatrick@gmail.com>
2015-11-21 01:34:42 -0800

    implemented email

--- getdata/config.py
+++ getdata/config.py
@@ -1,9 +1,9 @@
 
-MAIL_HOST = 'smtp.googlemail.com'
+MAIL_SERVER = 'smtp.googlemail.com'
 MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
-MAIL_HOSTNAME = 'myuciclassisfull'
+MAIL_USERNAME = 'myuciclassisfull@gmail.com'
 MAIL_PASSWORD = '#Passwor'
 
 ADMINS = ['myuciclassisfull@gmail.com']
--- getdata/jqueryexample.py
+++ getdata/jqueryexample.py
@@ -11,10 +11,11 @@
 from flask import Flask, jsonify, render_template, request
 import urllib2
 from flask.ext.mail import Mail,Message
-from config import ADMINS, MAIL_HOST,MAIL_PORT,MAIL_HOSTNAME,MAIL_PASSWORD,MAIL_USE_TLS,MAIL_USE_SSL
+from config import ADMINS#, MAIL_SERVER,MAIL_PORT,MAIL_HOSTNAME,MAIL_PASSWORD,MAIL_USE_TLS,MAIL_USE_SSL
 from threading import Thread
 
 app = Flask(__name__)
+app.config.from_object('config')
 mail = Mail(app)
 
 @app.route('/_add_numbers')
@@ -27,18 +28,19 @@ def add_numbers():
     if found == -1:
         waitlist = html.find("Waitl")
         if waitlist == -1:
-            send_email()
+            send_email(a)
             return jsonify(result="OPEN")
         else:
             return jsonify(result="Waitl")
     else:
+        #add to database here
         return jsonify(result="FULL")
 
 #@app.route('/_send_email')
-def send_email():
-    msg = Message('Your class became available!', sender=ADMINS[0], recipients=ADMINS)
-    msg.body = 'Your class _ became available!'
-    msg.html = '<b>HTML</b> body'
+def send_email(courseID):
+    msg = Message('Your class {} became available!'.format(str(courseID)), sender=ADMINS[0], recipients=ADMINS)
+    msg.body = 'Your class {} became available!'.format(str(courseID))
+    #msg.html = '<b>HTML</b> body'
     thr = Thread(target=send_async_email,args=[app,msg])
     thr.start()
 
