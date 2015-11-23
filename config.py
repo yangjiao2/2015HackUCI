4aa1e6009b34a98f8ed1d4a5c998b8733b411603
J. Patrick Park <parkjpatrick@gmail.com>
2015-11-23 11:19:05 -0800

    Update config.py
    Adding clarification comment for the config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ MAIL_PORT = 465
 MAIL_USE_TLS = False
 MAIL_USE_SSL = True
 MAIL_USERNAME = 'myuciclassisfull@gmail.com'
-MAIL_PASSWORD = '#Passwor'
+MAIL_PASSWORD = '#Passwor'  #No worries, we are actually using a different password for the email account!
 
 ADMINS = ['myuciclassisfull@gmail.com']
